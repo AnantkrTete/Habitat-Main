44f4ee508d6bd793ce294137d913dac44cd68fee
AnantkrTete <135524288+AnantkrTete@users.noreply.github.com>
2024-08-31 13:57:30 +0530

    dynamic year

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import "./footer.css"
 import React from 'react';
 
+var d = new Date();
+var currYear = d.getFullYear();
 
 export const Footer = () => {
   return (
     <div className="footer">
-            <div className="fText"> Copyright @ 2023 : Apna Nazariya --  by  Pawar-Ankur </div>
+            <div className="fText"> Copyright @ {currYear} : Apna Nazariya --  by  Anant </div>
     </div>
   )
 }
